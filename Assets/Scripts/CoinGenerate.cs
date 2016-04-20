261ede0ddb58664764130e5c709876d668ada4e5
Husni Munaya <husnimun@gmail.com>
2016-04-20 15:03:11 +0700

    add coin

---
+++ Assets/Scripts/CoinGenerate.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinGenerate : MonoBehaviour {
+
+	public GameObject coin;
+
+	public static float positionCoinLast;
+	public static float positionCoinAwake;
+	float timeElapsed = 0;
+	float spawnCycle = 2f;
+
+
+	// Use this for initialization
+	void Start () {
+		positionCoinLast = 0f;
+		positionCoinAwake = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeElapsed += Time.deltaTime;
+		if (timeElapsed > spawnCycle) {
+			GameObject temp = Instantiate(coin) as GameObject;
+			Vector3 pos = temp.transform.position;
+			temp.transform.position = new Vector3 (generateRandomPosition(),pos.y,positionCoinAwake + 20);
+			timeElapsed -= spawnCycle;
+		}
+	}
+
+	float generateRandomPosition(){
+		float val = Random.Range(-3,3);
+		if(val < -1) return -3;
+		else if(val <= 1) return 0;
+		else return 3;
+	}
+}
---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinPickup : MonoBehaviour {
+
+	public Transform coinEffect;
+
+	void Update(){
+		if (transform.position.z < CoinGenerate.positionCoinLast)
+			Destroy (gameObject);
+	}
+
+	void OnTriggerEnter(Collider info){
+		if (info.tag == "Player") {
+			Transform effect = Instantiate (coinEffect,transform.position, transform.rotation) as Transform;
+			Destroy (effect.gameObject, 3);
+			Destroy (gameObject);
+		}
+	}
+}
