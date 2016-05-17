9973928fe011908884ea82aa2fb584ced125e3e9
Husni Munaya <husnimun@gmail.com>
2016-05-17 22:49:16 +0700

    add untracked file

---
+++ Assets/Scripts/StoryScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoryScript : MonoBehaviour {
+
+    public GameObject image;
+
+	void Start () {
+	
+	}
+	
+	
+	void FixedUpdate () {
+        image.transform.Translate(0, 2, 0);    
+	}
+}
